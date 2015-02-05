5e88d78e2640f07e513ddc364158383c8a841efa
taylormurphy <taylor@transcriptic.com>
2015-02-05 05:12:41 +0000

    Adding codesittersgit

---
+++ CodeSitters.py
@@ -0,0 +1,7 @@
+x=1
+while x<25:                                                                                                                                                         
+     if x==13:                                                                                                                                                        
+             print "GUMMY BEARS"                                                                                                                                          
+     else:                                                                                                                                                           
+             print x                                                                                                                                                 
+     x=x+1 
